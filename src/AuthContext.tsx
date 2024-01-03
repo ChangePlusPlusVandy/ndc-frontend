46c358d56330c2bb2b6d831988349c96a50fff8c
kai dubauskas <kaidubauskas@gmail.com>
2024-01-03 16:51:49 -0600

    complete auth for registration

--- src/AuthContext.tsx
+++ src/AuthContext.tsx
@@ -18,9 +18,11 @@ interface AuthContextData {
     name: string,
     email: string,
     password: string,
+    isStaff: boolean,
   ) => Promise<void>;
   logout: () => Promise<void>;
   getUser: () => User | null;
+  isStaff: boolean | null;
   forgotPassword: (email: string) => Promise<void>;
   confirmReset: (code: string, password: string) => Promise<void>;
 }
@@ -34,23 +36,84 @@ export function useAuth(): AuthContextData {
 export function AuthProvider({ children }: { children: React.ReactNode }) {
   const [currentUser, setCurrentUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [isStaff, setIsStaff] = useState<boolean | null>(null);
 
   async function login(email: string, password: string) {
     return await signInWithEmailAndPassword(auth, email, password);
+
+    //TODO: HANDLE LOGIN
   }
 
-  async function registerUser(name: string, email: string, password: string) {
+  async function registerUser(name: string, email: string, password: string, isStaff: boolean) {
     return await createUserWithEmailAndPassword(auth, email, password).then(
       (userCredential) => {
         void updateProfile(userCredential.user, {
           displayName: name,
         });
       },
-    );
+    ).then(() => {
+      isStaff ? createMongoStaff(name, email) : createMongoPartner(name, email)
+    })
+  };
+
+  const createMongoStaff = async (name: string, email: string) => {
+    try {
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: `Bearer ${window.localStorage.getItem("auth")}`,
+        },
+        body: JSON.stringify({
+          firstName: name,
+          lastName: name,
+          email: email,
+          phoneNumber: "123",
+          firebaseUID: currentUser?.uid
+        })
+      }
+      const res = await fetch("/api/staff/", requestOptions);
+      const staffUser = await res.json()
+      setIsStaff(true);
+      console.log("NEW STAFF, ", staffUser);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  const createMongoPartner = async (name: string, email: string) => {
+    try {
+      const requestOptions = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          // Authorization: `Bearer ${window.localStorage.getItem("auth")}`,
+        },
+        body: JSON.stringify({
+          firstName: name,
+          lastName: name,
+          email: email,
+          phoneNumber: "123",
+          location: "ld",
+          address: "slkdjf",
+          numOrdersTotal: 1,
+          numOrdersYTD: 1,
+          numOrdersMonth: 1,
+          type: "DFD",
+          firebaseUID: currentUser?.uid
+        })
+      }
+      const res = await fetch("/api/partner/", requestOptions);
+      const partnerUser = await res.json()
+      setIsStaff(false);
+      console.log("NEW Partner, ", partnerUser)
+    } catch (err) {
+      console.error(err);
+    }
   }
 
   async function logout(): Promise<void> {
     return await signOut(auth);
+    setIsStaff(null)
   }
 
   function getUser(): User | null {
@@ -73,6 +136,22 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     return unsubscribe;
   }, []);
 
+  const setToken = async () => {
+    const userToken = await currentUser?.getIdToken();
+    if (userToken) {
+      window.localStorage.setItem("auth", userToken);
+    }
+  };
+
+  useEffect(() => {
+    if (currentUser) {
+      setToken()
+      console.log("SAVED TOKEN ", window.localStorage.getItem("auth"))
+    } else {
+      window.localStorage.removeItem("auth");
+    }
+  }, [currentUser])
+
 
   const value = {
     currentUser,
@@ -80,6 +159,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     registerUser,
     logout,
     getUser,
+    isStaff,
     forgotPassword,
     confirmReset,
   };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,13 +2,30 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useAuth } from "../AuthContext";
 
+interface DiaperSizes {
+  newborn: number;
+  size1: number;
+  size2: number;
+  size3: number;
+  size4: number;
+  size5: number;
+  size6: number;
+}
+
+interface InventoryApiResponse {
+  _id: {
+    $oid: string;
+  };
+  wrapped: DiaperSizes;
+  unwrapped: DiaperSizes;
+}
+
 const Home: React.FC = () => {
-  const [fact, setFact] = useState<string>("");
+  const [inventory, setInventory] = useState<InventoryApiResponse | null>(null)
   const { currentUser } = useAuth();
 
   useEffect(() => {
     const fetchFact = async () => {
-      console.log("called");
       try {
         const token = await currentUser?.getIdToken();
 
@@ -22,22 +39,42 @@ const Home: React.FC = () => {
         // below, the /api is replaced with the server url defined in vite.config.ts
         // so, if the server is defined as "localhost:3001" in that file,
         // the fetch url will be "localhost:3001/example"
-        const res = await fetch("/api/example", payloadHeader);
-        setFact(await res.text());
+        const res = await fetch("/api/inventory/", payloadHeader);
+        const data = await res.json() as InventoryApiResponse;
+        setInventory(data);
       } catch (err) {
         console.log(err);
       }
     };
-
+    console.log("user", currentUser)
     void fetchFact();
+    console.log("inventory", inventory)
   }, [currentUser]);
+
+
 
   return (
     <div>
-      This is a React Firebase Auth template. Below is a fact from a protected
-      route on the server.
-
-      <p>{fact}</p>
+      <div>
+        <h2>wrapped:</h2>
+        <h4>newborn: {inventory?.wrapped.newborn} </h4>
+        <h4>size1: {inventory?.wrapped.size1}</h4>
+        <h4>size2: {inventory?.wrapped.size2}</h4>
+        <h4>size3: {inventory?.wrapped.size3}</h4>
+        <h4>size4: {inventory?.wrapped.size4}</h4>
+        <h4>size5: {inventory?.wrapped.size5}</h4>
+        <h4>size6: {inventory?.wrapped.size6}</h4>
+      </div>
+      <div>
+        <h2>Unwrapped: </h2>
+        <h4>newborn: {inventory?.unwrapped.newborn}</h4>
+        <h4>size1: {inventory?.unwrapped.size1}</h4>
+        <h4>size2: {inventory?.unwrapped.size2}</h4>
+        <h4>size3: {inventory?.unwrapped.size3}</h4>
+        <h4>size4: {inventory?.unwrapped.size4}</h4>
+        <h4>size5: {inventory?.unwrapped.size5}</h4>
+        <h4>size6: {inventory?.unwrapped.size6}</h4>
+      </div>
       <br />
       <Link to="/profile">Profile</Link>
     </div>
