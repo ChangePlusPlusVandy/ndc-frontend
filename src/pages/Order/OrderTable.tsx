d9ebba8ce6f021eda91fe9096a76159bfb0049fd
Kai Dubauskas <kaidubauskas@gmail.com>
2024-01-22 17:51:20 -0600

    add mongoId to authContext

--- src/pages/Order/OrderTable.tsx
+++ src/pages/Order/OrderTable.tsx
@@ -2,33 +2,14 @@ import React, { useEffect, useState } from 'react';
 import Order from './OrderClass';
 import "./OrderPartner.css";
 
+
 interface TableProps {
     orders: Order[],
     orderType: string,
 }
 
 const OrderTable: React.FC<TableProps> = ({ orders, orderType }: TableProps) => {
-    useEffect(() => {
-        const getOrders = async () => {
-            fetch(`${import.meta.env.VITE_BACKEND_URL}/INSERT_PATH_HERE`, {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(orderType),
-            })
-                .then((response) => (response.json()))
-                .then((data) => {
-                    orders = [];
-                    data.forEach((elem: any) => {
-                        orders.push(new Order(elem.datePlaced, elem.status, elem.newborn, elem.size1,
-                            elem.size2, elem.size3, elem.size4, elem.size5, elem.size6));
-                    })
-                })
-                .catch(console.error);
-        }
-        // getOrders();
-    }, [])
+
 
     return (
         <div>
