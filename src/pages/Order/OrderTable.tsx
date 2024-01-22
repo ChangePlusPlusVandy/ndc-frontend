12dfdd5f4f0663e8335031bdd7b752ea077d517c
Kai Dubauskas <kaidubauskas@gmail.com>
2024-01-22 12:40:31 -0600

    add navbar and format partner dash

--- src/pages/Order/OrderTable.tsx
+++ src/pages/Order/OrderTable.tsx
@@ -1,65 +1,64 @@
-import React, {useEffect, useState} from 'react';
-import Order from './OrderClass'; 
-import "./OrderPartner.css";  
+import React, { useEffect, useState } from 'react';
+import Order from './OrderClass';
+import "./OrderPartner.css";
 
 interface TableProps {
-    orders: Order[], 
+    orders: Order[],
     orderType: string,
 }
 
-const OrderTable: React.FC<TableProps> = ({orders, orderType}: TableProps) => {
-
+const OrderTable: React.FC<TableProps> = ({ orders, orderType }: TableProps) => {
     useEffect(() => {
         const getOrders = async () => {
-            fetch("${import.meta.env.VITE_BACKEND_URL}/INSERT_PATH_HERE", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json",
-                    },
-                    body: JSON.stringify(orderType),
-                })
+            fetch(`${import.meta.env.VITE_BACKEND_URL}/INSERT_PATH_HERE`, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(orderType),
+            })
                 .then((response) => (response.json()))
-                .then ((data) => {
-                    orders = []; 
+                .then((data) => {
+                    orders = [];
                     data.forEach((elem: any) => {
-                        orders.push(new Order(elem.datePlaced, elem.status, elem.newborn, elem.size1, 
-                            elem.size2, elem.size3, elem.size4, elem.size5, elem.size6)); 
-                    }) 
+                        orders.push(new Order(elem.datePlaced, elem.status, elem.newborn, elem.size1,
+                            elem.size2, elem.size3, elem.size4, elem.size5, elem.size6));
+                    })
                 })
-                .catch(console.error); 
+                .catch(console.error);
         }
-        getOrders(); 
+        // getOrders();
     }, [])
 
     return (
-        <table>
-                <thead>
-                    <tr>
-                        <th>Order</th>
-                        <th>Date Placed</th>
-                        <th>Number of Diapers</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {orders?.map((val: Order, index: number) => {
-                        return (
-                            <tr key={index} className="backWhite">
-                                <td>Order</td>
-                                <td>{val.datePlaced.toDateString()}</td>
-                                <td>{val.numDiapers}</td>
-                                <td>...</td>
-                            </tr>
-                        )
-                    })}
-                    <tr>
-                        <td>temp</td>
-                        <td>temp</td>
-                        <td>temp</td>
-                    </tr>
-                </tbody>
-                
-        </table>
-    )
+        <div>
+            <div>
+                <div>
+                    <p>Order</p>
+                    <p>Date Placed</p>
+                    <p>Number of Diapers</p>
+                </div>
+            </div>
+            <div>
+                {orders?.map((val: Order, index: number) => {
+                    return (
+                        <p key={index} className="backWhite">
+                            <p>Order</p>
+                            <p>{val.datePlaced.toDateString()}</p>
+                            <p>{val.numDiapers}</p>
+                            <p>...</p>
+                        </p>
+                    )
+                })}
+                <div>
+                    <p>temp</p>
+                    <p>temp</p>
+                    <p>temp</p>
+                </div>
+            </div>
+
+        </div>
+    );
 }
 
 export default OrderTable; 
--- src/pages/PartnerDashboard/MakeOrderBtn.tsx
+++ src/pages/PartnerDashboard/MakeOrderBtn.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import { Button, Text, Title, Image, Flex}  from '@mantine/core';
-import placeholder from './Images/placeholder.png';
+import { Button, Text, Title, Image, Flex } from '@mantine/core';
+import placeholder from '../../assets/placeholder.png';
 
 interface MakeOrderButtonProps {
     handleOnClick: () => void;
 }
 
-const MakeOrderBtn: React.FC<MakeOrderButtonProps> = ({handleOnClick}) =>{
+const MakeOrderBtn: React.FC<MakeOrderButtonProps> = ({ handleOnClick }) => {
     return (
-        <Button onClick={handleOnClick} radius = "xs" variant = "filled" color = "gray" size = "md" h = {70}>
-            <Flex direction = "column" justify = "center" align = "center">
-                <Image  mt = {2} h = {20} w = {20} src = {placeholder} alt = "Placeholder" />
-                <Text fw = {500}>
+        <Button onClick={handleOnClick} radius="xs" variant="filled" color="gray" size="md" h={70}>
+            <Flex direction="column" justify="center" align="center">
+                <Image mt={2} h={20} w={20} src={placeholder} alt="Placeholder" />
+                <Text fw={500}>
                     Make Order
                 </Text>
             </Flex>
-            
+
         </Button>
     );
 };
