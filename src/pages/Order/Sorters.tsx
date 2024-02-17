3136fb7d80ca6a397870b8fe6701c3a07d50f7ce
Jason Lee <jaeho52lee@gmail.com>
2024-02-17 14:44:28 -0600

    made sorters standalone

--- src/pages/Order/Sorters.tsx
+++ src/pages/Order/Sorters.tsx
@@ -1,36 +1,97 @@
 import { Menu, Button, Image, rem } from '@mantine/core';
 import { IconChevronDown } from '@tabler/icons-react';
-import { MouseEventHandler } from 'react';
+import Order from './OrderClass';
 
 interface SorterProps {
-    sortDate: MouseEventHandler,
-    sortNum: MouseEventHandler
+    orders: Order[], 
+    setOrders: React.Dispatch<React.SetStateAction<Order[]>>
+    whichSorters: string[],
+    classes: string,
 }
-<div className="page-container">
-    <h1>Page Title</h1>
-    <div className="page-header">
-        <h1>Header title</h1>
-        <h3>Sub-title</h3>
-        <img id="logo"></img>
-    </div>
-</div>
 
+const Sorter: React.FC<SorterProps> = ({ orders, setOrders, whichSorters, classes}: SorterProps) => {
+    const statusRank: Map<string, number> = new Map([
+        ["CANCELLED", 0],
+        ["PLACED", 1],
+        ["OPEN", 2],
+        ["FILLED", 3]
+    ])
 
-const Sorter: React.FC<SorterProps> = ({ sortDate, sortNum }: SorterProps) => {
+    const sortOrderName = () => {
+        let orderCopy: Order[] = orders.slice(); 
+        orderCopy.sort((a, b) => (a.id < b.id ? -1 : 1)); 
+        setOrders(orderCopy); 
+    }
+
+    const sortPartnerName = () => {
+        let orderCopy: Order[] = orders.slice(); 
+        orderCopy.sort((a, b) => (a.partner < b.partner ? -1 : 1)); 
+        setOrders(orderCopy);
+    }
+
+    const sortDate = () => {
+        let orderCopy: Order[] = orders.slice();
+        orderCopy.sort((a, b) => (a.datePlaced < b.datePlaced ? -1 : 1));
+        setOrders(orderCopy);
+    }
+
+    const sortNum = () => {
+        let orderCopy: Order[] = orders.slice();
+        orderCopy.sort((a, b) => (a.numDiapers < b.numDiapers ? -1 : 1));
+        setOrders(orderCopy);
+    }
+
+    const statusSorter = (a: Order, b: Order) => {
+        const aRank = statusRank.get(a.status); 
+        const bRank = statusRank.get(b.status); 
+
+        console.log(a.status + " " + aRank + " " + b.status + " " + bRank); 
+        
+        if (!aRank) return -1; 
+        if (!bRank) return 1; 
+
+        return aRank < bRank ? -1 : 1; 
+    }
+
+    const sortStatus = () => {
+        let orderCopy: Order[] = orders.slice(); 
+        orderCopy.sort(statusSorter); 
+        setOrders(orderCopy); 
+    }
+    
     return (
         <>
             <Menu offset={0}>
                 <Menu.Target>
-                    <Button className='whiteButton' rightSection={<IconChevronDown></IconChevronDown>}>Sort By</Button>
+                    <Button className={classes} rightSection={<IconChevronDown></IconChevronDown>}>Sort By</Button>
                 </Menu.Target>
 
                 <Menu.Dropdown>
-                    <Menu.Item onClick={sortDate}>
-                        Sort by date
-                    </Menu.Item>
-                    <Menu.Item onClick={sortNum}>
-                        Sort by # of diapers
-                    </Menu.Item>
+                    {whichSorters.includes("OrderName") && 
+                        <Menu.Item onClick={sortOrderName}>
+                            Sort by Order Name
+                        </Menu.Item>
+                    }
+                    {whichSorters.includes("PartnerName") && 
+                        <Menu.Item onClick={sortPartnerName}>
+                            Sort by Partner Name
+                        </Menu.Item>
+                    }
+                    {whichSorters.includes("Date") && 
+                        <Menu.Item onClick={sortDate}>
+                            Sort by date
+                        </Menu.Item>
+                    }
+                    {whichSorters.includes("Num") && 
+                        <Menu.Item onClick={sortNum}>
+                            Sort by # of diapers
+                        </Menu.Item>
+                    }
+                    {whichSorters.includes("Status") && 
+                        <Menu.Item onClick={sortStatus}>
+                            Sort by Status
+                        </Menu.Item>
+                    }
                 </Menu.Dropdown>
             </Menu>
 
