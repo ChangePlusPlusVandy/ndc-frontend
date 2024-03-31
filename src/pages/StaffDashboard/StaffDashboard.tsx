631339ac1be83936f43d42705679a2efc35dd06b
Charles Poulin <charlespoulin@Charless-MacBook-Pro.local>
2024-03-31 16:52:56 -0500

    format code

--- src/pages/StaffDashboard/StaffDashboard.tsx
+++ src/pages/StaffDashboard/StaffDashboard.tsx
@@ -8,7 +8,6 @@ import { DonutChart, BarChart } from "@mantine/charts";
 import "../../styles/StaffDashboard.css";
 import { IconCircleFilled, IconCircle } from "@tabler/icons-react";
 
-import ConfirmAccountCreated from "../ConfirmAccountCreated";
 
 
 export interface InventoryResponse {
@@ -113,7 +112,6 @@ useEffect(() => {
   useEffect(() => {
       diapperDeliveredChart();
   },[deliveredData]);*/
-  const [isOpen, setIsOpen] = useState(false);
 
   const fakeDonutChart = [
     { name: "Unreviewed", value: 400, color: "var(--chart-light-color)" },
@@ -324,8 +322,6 @@ useEffect(() => {
           </Flex>
         </Grid.Col>
       </Grid>
-      <ConfirmAccountCreated isOpen={isOpen} setIsOpen={setIsOpen} newUser={{fullName:"HFGJ", userType:"Partner", email:"DFGHJ@gmail.com"}} />
-      <button onClick={() => setIsOpen(true)}/>
     </>
   );
 };
